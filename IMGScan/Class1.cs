470683f4f83522145a1ad3e4cc6fca1268d54cbf
DangouHaim <gholub1996@mail.ru>
2016-10-18 11:47:47 +0300

    c1

---
+++ IMGScan/Class1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMGScan
+{
+    public class Neuron
+    {
+        public string Name
+        {
+            get { return _name; }
+        }
+        private string _name;
+        double[][] _in;
+        double[][] _w;
+        double _out;
+
+        double _max = 1500;
+        double _learnSpeed = 0.1;
+        double _min = 0;
+
+
+        public Neuron(int w, int h, string name)
+        {
+            _name = name;
+            _in = new double[w][];
+            _w = new double[w][];
+            for (int i = 0; i < w; i++)
+            {
+                _in[i] = new double[h];
+                _w[i] = new double[h];
+            }
+
+            Init();
+
+        }
+
+        public void Init()
+        {
+            Random r = new Random();
+            for (int i = 0; i < _w.Length; i++)
+            {
+                for (int j = 0; j < _w[i].Length; j++)
+                {
+                    _w[i][j] = r.Next(2) * 0.3 + 0.1;
+                }
+            }
+        }
+
+        public double Outer()
+        {
+            _out = 0;
+
+            for (int i = 0; i < _in.Length; i++)
+            {
+                for (int j = 0; j < _in[i].Length; j++)
+                {
+                    if (_in[i][j] < 200)
+                    {
+                        _out += _w[i][j] * _in[i][j];
+                    }
+                }
+            }
+
+            if (_out > _max)
+            {
+                _out = 1;
+            }
+            else
+            {
+                _out = 0;
+            }
+            return _out;
+        }
+
+        public void Study()
+        {
+            double ge = 0;
+            do
+            {
+                ge = 0;
+                foreach (var v in Directory.GetFiles("Patterns/" + _name))
+                {
+                    int answer = 0;
+                    if (v.IndexOf("true") > -1)
+                    {
+                        answer = 1;
+                    }
+
+                    Bitmap b = (Bitmap)Bitmap.FromFile(v);
+                    for (int i = 0; i < b.Width; i++)
+                    {
+                        for (int j = 0; j < b.Height; j++)
+                        {
+                            _in[i][j] = (b.GetPixel(i, j).R + b.GetPixel(i, j).G + b.GetPixel(i, j).B) / 3;
+                        }
+                    }
+                    Outer();
+                    double le = answer - _out;
+                    ge += Math.Abs(le);
+
+                    for (int i = 0; i < b.Width; i++)
+                    {
+                        for (int j = 0; j < b.Height; j++)
+                        {
+                            if (_in[i][j] < 200)
+                            {
+                                _w[i][j] += _learnSpeed * _in[i][j] * le;
+                            }
+                        }
+                    }
+                }
+            }
+            while (ge > _min);
+        }
+
+        public void LoadFromFile(string v)
+        {
+            Bitmap b = (Bitmap)Bitmap.FromFile(v);
+            for (int i = 0; i < b.Width; i++)
+            {
+                for (int j = 0; j < b.Height; j++)
+                {
+                    _in[i][j] = (b.GetPixel(i, j).R + b.GetPixel(i, j).G + b.GetPixel(i, j).B) / 3;
+                }
+            }
+        }
+    }
+}
---
+++ IMGScan/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IMGScan
+{
+    public partial class Form1 : Form
+    {
+        Neuron[] n;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Init();
+        }
+
+        public void Init()
+        {
+            n = new Neuron[]
+            {
+                new Neuron(50, 50, "А"),
+                new Neuron(50, 50, "Б"),
+                new Neuron(50, 50, "В"),
+                new Neuron(50, 50, "Г"),
+                new Neuron(50, 50, "Д"),
+                new Neuron(50, 50, "Е"),
+                new Neuron(50, 50, "Ё"),
+                new Neuron(50, 50, "Ж"),
+                new Neuron(50, 50, "З"),
+                new Neuron(50, 50, "И"),
+                new Neuron(50, 50, "Й"),
+                new Neuron(50, 50, "К"),
+                new Neuron(50, 50, "Л"),
+                new Neuron(50, 50, "М"),
+                new Neuron(50, 50, "Н"),
+                new Neuron(50, 50, "О"),
+                new Neuron(50, 50, "П"),
+                new Neuron(50, 50, "Р"),
+                new Neuron(50, 50, "С"),
+                new Neuron(50, 50, "Т"),
+                new Neuron(50, 50, "У"),
+                new Neuron(50, 50, "Ф"),
+                new Neuron(50, 50, "Х"),
+                new Neuron(50, 50, "Ц"),
+                new Neuron(50, 50, "Ч"),
+                new Neuron(50, 50, "Ш"),
+                new Neuron(50, 50, "Щ"),
+                new Neuron(50, 50, "Ь"),
+                new Neuron(50, 50, "Ы"),
+                new Neuron(50, 50, "Ъ"),
+                new Neuron(50, 50, "Э"),
+                new Neuron(50, 50, "Ю"),
+                new Neuron(50, 50, "Я")
+            };
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < n.Length; i++)
+            {
+                n[i].Study();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog o = new OpenFileDialog())
+            {
+                if(o.ShowDialog() == DialogResult.OK)
+                {
+                    for(int i = 0; i < n.Length; i++)
+                    {
+                        n[i].LoadFromFile(o.FileName);
+                        if (n[i].Outer() > 0)
+                        {
+                            MessageBox.Show(n[i].Name);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
